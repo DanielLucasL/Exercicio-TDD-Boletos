3fe91891a6d0611c24a58418cbfe4527075b536d
DanielLucasL <72170522+DanielLucasL@users.noreply.github.com>
2022-04-18 14:21:10 -0300

    Commit 3

---
+++ tdd/src/Fatuta/Fatura.java
@@ -0,0 +1,35 @@
+package src.Fatuta;
+
+public class Fatura {
+
+	public void setnomedocliente(String nomedocliente) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setdata(String data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setvalortotal(double valortotal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object getnomedocliente() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getdata() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getvalortotal() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- tdd/test/Fatura/FaturaTest.java
+++ tdd/test/Fatura/FaturaTest.java
@@ -5,10 +5,35 @@
  */
 package test.Fatura;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import src.Fatuta.Fatura;
+
 /**
  *
  * @author Aluno
  */
 public class FaturaTest {
+    Fatura fatura;
+@BeforeEach
+public void inicializa() {
+       fatura = new Fatura();
+}
+
+
+
+@Test
+public void testChegararmazenamento() {
+	fatura.setnomedocliente("Daniel");
+	fatura.setdata("04/02/2009");
+	fatura.setvalortotal(50000.00);
+    assertAll("boleto", () -> assertEquals("A32B", fatura.getnomedocliente()),
+            () -> assertEquals("14/02/2009", fatura.getdata()), () -> assertEquals("50000.0", fatura.getvalortotal().toString()));
+            
+}
     
 }
